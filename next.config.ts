b9fcc9ec542f1225c238a1ea3412bf8dd060654e
akira2709 <145766714+akira2709@users.noreply.github.com>
2025-07-02 21:14:04 +0300

    microfix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  env: {
+    API_URL: process.env.API_URL,
+    NEXT_PUBLIC_GITHUB_CLIENT_ID: process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID,
+    NEXT_PUBLIC_GITHUB_CALLBACK_URL: process.env.NEXT_PUBLIC_GITHUB_CALLBACK_URL,
+    NEXT_PUBLIC_GOOGLE_CLIENT_ID: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+    NEXT_PUBLIC_GOOGLE_CALLBACK_URL: process.env.NEXT_PUBLIC_GOOGLE_CALLBACK_URL,
+  },
 };
 
 export default nextConfig;
